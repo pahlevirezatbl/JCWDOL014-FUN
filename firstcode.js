fcf05255391617c27450f3f486b8c775b70ff969
pahlevirezatbl <pahlevirezatbl7@gmail.com>
2024-05-05 13:58:04 +0700

    first init

---
+++ firstcode.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+// ini catatan
+
+/*ini catatan
+2 baris atau lebih*/
+
+let message; // membuat variable bernama message
+message = "hello" // mengisi nilai kedalam variable message
+
+
+var globalvariable = "hello world!"
+
+let replaceable = "this value will be replaced"
+replaceable = "i love javascript"
+
+const constant = "hello purwadhika"
+constant = "hello world"
+
+console.log(globalvariable)
+console.log(replaceable)
+console.log(constant)
